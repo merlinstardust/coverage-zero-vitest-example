694fa0fcc64c5de2f80d0815fd311d75b8ba12bb
Merlin (they/them) <421960+merlinpatt@users.noreply.github.com>
2023-06-26 20:33:31 -0400

    Update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,37 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { configDefaults } from 'vitest/config';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+export default defineConfig(async () => ({
+  plugins: [vue()],
   resolve: {
     alias: {
-      '~': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
+  test: {
+    coverage: {
+      include: ['src/components/*', 'src/store/*'],
+      exclude: [
+        ...configDefaults.coverage.exclude,
+        // This is likely to contain bootstrapping code, which is harder and less-valuable to test
+        'src/main.ts',
+
+        'src/App.vue',
+        'src/styles/*',
+        // Icons are basic SVGs and don't need testing
+        'src/icons/*',
+        // Screens are made up of tested base components
+        'src/screens/*',
+      ],
+      provider: 'v8',
+      // FIXME: The text appears twice in the console output, but zero times if I remove "text"
+      reporter: ['cobertura', 'lcov', 'text'],
+    },
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: 'vitest.setup.ts',
+  },
+}));
